4c99ce0e2306078004b5de9e082319a8a038bf54
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-02-13 09:39:59 +0530

    asignment done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Abhinav V</h1>
+      <p>Contact: Abhinav.v.s66@kalvium.community</p>
+      <p>Phone: 1231231231</p>
+    </div>
+  );
+}
+
+export default App;
